e5c556e920d6ed62cdb403c42f1c4f67af7d6397
Elvina Ganieva <elvinaganieva5411@yandex.ru>
2021-11-21 16:53:49 +0300

    add db

---
+++ message-system/src/main/kotlin/com/example/retailer/adapter/Impl.kt
@@ -0,0 +1,30 @@
+package com.example.retailer.adapter
+
+import com.example.retailer.api.distributor.Order
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.amqp.core.Message
+import org.springframework.amqp.core.MessagePostProcessor
+import org.springframework.amqp.core.TopicExchange
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Component
+
+@Component("distributorPublisher")
+class Impl : DistributorPublisher {
+    @Autowired
+    private lateinit var template: RabbitTemplate
+
+    @Autowired
+    private lateinit var topic: TopicExchange
+
+    override fun placeOrder(order: Order): Boolean {
+        val objectMapper = ObjectMapper()
+        val message = objectMapper.writeValueAsString(order)
+        template.convertAndSend(message, topic.name) { m: Message ->
+            m.messageProperties.headers["Notify-Exchange"] = "distributor_exchange"
+            m.messageProperties.headers["Notify-RoutingKey"] = "retailer.myGitHub.#"
+            m
+        }
+        return true
+    }
+}
